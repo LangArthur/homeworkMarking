fb7a5a3f35d4a56a5f87300f26fa935be21bddf7
alang <arthur.lang@epitech.eu>
2021-03-09 17:08:25 +0100

    add basic training with weights on Mnist dataset

---
+++ HomeworkMarking.py
@@ -0,0 +1,63 @@
+#
+# Created on Tue Mar 09 2021
+#
+# Arthur Lang
+# HomeworkMarking.py
+#
+
+from enum import Enum
+import tensorflow
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
+
+# class HMMode(Enum):
+#     TRAINING = 0
+#     RUNNING = 1
+
+class HomeworkMarking():
+
+    def __init__(self, weightPath = None):
+        # self._mode = HMMode.TRAINING
+        self._model = self.buildModel()
+        if (weightPath != None):
+            self._model.load_weights(weightPath)
+            self._hasWeight = True
+        else:
+            self._hasWeight = False
+        # self._action = {
+        #     HMMode.TRAINING: self._train,
+        #     HMMode.RUNNING: self._run
+        # }
+        self.outputWeightPath = "logs/checkpoints/cp1.ckpt"
+
+    def setOutputWeightPath(self, path):
+        self.outputWeightPath = path
+
+    def setMode(self, newMode):
+        self.mode = newMode
+
+    def buildModel(self):
+        model = Sequential([
+            Conv2D(filters = 32, kernel_size=(3,3), activation='relu'),
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPool2D(2, 2),
+            Dropout(0.25),
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dropout(0.5),
+            Dense(10, activation='softmax'),
+        ])
+
+        model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+
+    def train(self, data, labels, dataTest, labelTest):
+        cpCallBack = tensorflow.keras.callbacks.ModelCheckpoint(filepath=self.outputWeightPath, save_weights_only=True, verbose=1)
+        self._model.fit(data, labels, batch_size=128, epochs = 10, validation_data=(dataTest, labelTest), callbacks=[cpCallBack])
+
+    def run(self, data, labels, dataTest, labelTest):
+        self._model.fit(data, labels, batch_size=128, epochs = 10, validation_data=(dataTest, labelTest))
+
+    def evaluate(self, testData, testLabel):
+        # self._model.summary()
+        self._model.evaluate(testData, testLabel)
--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3
 
 import numpy
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
+import tensorflow
 from tensorflow.keras.datasets import mnist
 
-def main():
-    (dataTrain, labelTrain), (dataTest, labelTest) = mnist.load_data()
+from HomeworkMarking import *
 
+def dataPreprocessing():
+    (dataTrain, labelTrain), (dataTest, labelTest) = mnist.load_data()
     # normalize datas
     maxVal = numpy.max(dataTrain)
     dataTrain = (dataTrain / maxVal).reshape(60000, 28, 28, 1)
     dataTest = (dataTest / maxVal).reshape(10000, 28, 28, 1)
+    return dataTrain, labelTrain, dataTest, labelTest
 
-    #building model
-    model = Sequential()
-    model.add(Conv2D(filters = 32, kernel_size=(3,3), activation='relu'))
-    # model.add(Conv2D(filters = 32, kernel_size=(3,3), activation='relu', input_shape=dataTrain[0].shape()))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPool2D(2, 2))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
+# def main():
+#     (dataTrain, labelTrain), (dataTest, labelTest) = mnist.load_data()
 
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(dataTrain, labelTrain, batch_size=128, epochs = 10, validation_data=(dataTest, labelTest))
-    model.summary()
-    model.evaluate(dataTest, labelTest)
+#     # normalize datas
+#     maxVal = numpy.max(dataTrain)
+#     dataTrain = (dataTrain / maxVal).reshape(60000, 28, 28, 1)
+#     dataTest = (dataTest / maxVal).reshape(10000, 28, 28, 1)
+
+#     #building model
+#     model = buildModel()
+
+#     model.load_weights("logs/checkpoints/cp1.ckpt")
+
+#     # cpCallBack = tensorflow.keras.callbacks.ModelCheckpoint(filepath="logs/checkpoints/cp1.ckpt", save_weights_only=True, verbose=1)
+
+#     model.fit(dataTrain, labelTrain, batch_size=128, epochs = 10, validation_data=(dataTest, labelTest), callbacks=[cpCallBack])
+#     model.summary()
+#     model.evaluate(dataTest, labelTest)
+#     return 0
+
+def main():
+    dataTrain, labelTrain, dataTest, labelTest = dataPreprocessing()
+
+    hm = HomeworkMarking("logs/checkpoints/cp1.ckpt")
+    print("\033[92m Evaluate the Model \033[0m")
+    hm.evaluate(dataTest, labelTest)
     return 0
 
 if __name__ == "__main__":
