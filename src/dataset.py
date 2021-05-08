2d62cc1047b9e0a22046715b7641db8357240e51
alang <arthur.lang@epitech.eu>
2021-05-08 15:32:20 +0200

    merge

--- src/dataset.py
+++ src/dataset.py
@@ -29,20 +29,11 @@ def load_dataset(folder):
                 img = cv2.imread(os.path.join(images_path, image), cv2.IMREAD_GRAYSCALE)
                 data.append(img)
                 label.append(int(digitFolder))
-                # # push to train tdataset
-                # if item == "train" and img is not None:
-                #     images_train.append(img)
-                #     labels_train.append(int(number))
-                # # push to test tdataset
-                # elif item == "test" and img is not None:
-                #     images_test.append(img)
-                #     labels_test.append(int(number))
-    # return  images_train, labels_train, images_test, labels_test
     return data, label
+
 def data_augmenation(currentPath, augmentedImagesPath):
 
     resultsPath=os.path.join(augmentedImagesPath, "AugmentedDataset")
-    print("results path:",resultsPath)
     if(not os.path.exists(resultsPath)):
         os.mkdir(resultsPath)
     
@@ -59,17 +50,11 @@ def data_augmenation(currentPath, augmentedImagesPath):
                 img = load_img(imagePath, color_mode="grayscale")
                 # convert to numpy array
                 data = img_to_array(img)
-                print(data.shape)
                 # expand dimension to one sample
                 samples = expand_dims(data, 0)
-                print(samples.shape)
-                # img = cv2.imread(os.path.join(imagesPath,image), cv2.IMREAD_GRAYSCALE)
-                # print(type(img))
-                # img.astype('float32')
                 shift=0.2
                 datagen=igen(width_shift_range=shift, height_shift_range=shift,zca_whitening=True)
                 fit = datagen.fit(samples)
-                print("path to save:",pathToSave)
                 for batch in datagen.flow(samples, batch_size = 1, 
                       save_to_dir =pathToSave,  
                       save_prefix ='digit{}'.format(i), save_format ='jpeg'):
@@ -78,30 +63,6 @@ def data_augmenation(currentPath, augmentedImagesPath):
                             if i > 5: 
                                 i=0
                                 break
-                # image = np.squeeze(image)
-                # pyplot.imshow(fit.astype('uint8'))
-                # break
-                # datagen=igen(zca_whitening=True)
-                # img=datagen.fit(img)
-                # zca_whitening=True
-                # cv2.imwrite(os.path.join(resultsImagesPath , 'augmented{}.jpg'.format(i)), img)
-                
-
-    #    print(folder)
-    #    if isinstance(5, int)
-    #    images_path = os.path.join(folder, digitFolder)
-    #    resultsImagesPath=os.path.join(rasultsPath,digitFolder)
-    #    for image in os.listdir(images_path):
-    #        img = cv2.imread(os.path.join(images_path,image), cv2.IMREAD_GRAYSCALE)
-    #        img.astype('float32')
-    #        shift=0.2
-    #        datagen=igen(width_shift_range=shift, height_shift_range=shift)
-    #        fit = datagen.fit(img)
-    #        datagen=igen(zca_whitening=True)
-    #        img=datagen.fit(img)
-    #        zca_whitening=True
-    #        cv2.imwrite(os.path.join(resultsImagesPath , 'waka{}.jpg'.format(i)), img)
-
 
 ## set_shape
 # preprocess pictures from children to make them fit with mnist dataset
@@ -130,7 +91,3 @@ def load(preprocess = False):
         return  set_shape(data), labels
     else:
         return load_dataset("resources/datasets/Children_dataset")
-
-# data_augmenation("C:\\Users\\Madalina Aldea\\Desktop\\homeworkMarking\\resources\\datasets\\Children_dataset\\", "C:\\Users\\Madalina Aldea\\Desktop\\homeworkMarking\\resources\\datasets\\")
-# print(os.path.isdir("C:\\Users\\Madalina Aldea\\Desktop\\homeworkMarking\\resources\\datasets\\Children_dataset"))
-# os.mkdir("C:\\Users\\Madalina Aldea\\Desktop\\homeworkMarking\\resources\\datasets\\Children_dataset\\test")
